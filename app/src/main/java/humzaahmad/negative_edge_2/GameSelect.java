d82f1dde067cb8cd1d3b925253051152e5778b0f
Humza <ahmadhumza7@gmail.com>
2017-09-22 17:36:33 -0400

    Initial commit

---
+++ app/src/main/java/humzaahmad/negative_edge_2/GameSelect.java
@@ -0,0 +1,13 @@
+package humzaahmad.negative_edge_2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GameSelect extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_select);
+    }
+}
